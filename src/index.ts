e8540c246c5e3f17e62a22b3604488c67f157d62
Chris Braddock <chris@bluera.ai>
2025-12-16 16:47:10 -0500

    feat: add store CLI commands

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-import { createProgram } from './cli/program.js';
+import { createProgram, getGlobalOptions } from './cli/program.js';
+import { createStoreCommand } from './cli/commands/store.js';
 
 const program = createProgram();
+
+program.addCommand(createStoreCommand(() => getGlobalOptions(program)));
 
 program.parse();
