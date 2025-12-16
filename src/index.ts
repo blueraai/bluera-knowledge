9af25cc431fa0dc85c43e0649b1085eaa3bf93a4
Chris Braddock <chris@bluera.ai>
2025-12-16 16:49:32 -0500

    feat: add index CLI command

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@
 import { createProgram, getGlobalOptions } from './cli/program.js';
 import { createStoreCommand } from './cli/commands/store.js';
 import { createSearchCommand } from './cli/commands/search.js';
+import { createIndexCommand } from './cli/commands/index-cmd.js';
 
 const program = createProgram();
 
 program.addCommand(createStoreCommand(() => getGlobalOptions(program)));
 program.addCommand(createSearchCommand(() => getGlobalOptions(program)));
+program.addCommand(createIndexCommand(() => getGlobalOptions(program)));
 
 program.parse();
